03316173ee55352c045ec1c73f5903ae37598f0d
Jeangowhy <254141203@qq.com>
2025-04-27 12:20:07 +0800

    这是 Windows 系统掉进 BitLocker 黑洞时，我正在进行的文档工作，主要是 Elm 函数式编程。

--- utilities/kakasi.py
+++ utilities/kakasi.py
@@ -4,7 +4,7 @@ import sys
 import pykakasi
 kks = pykakasi.kakasi()
 text = "日本語を勉強します"
-macro = "ruby" if "ruby" in sys.argv else "o"
+macro = "ruby" if "ruby" in sys.argv else "oo"
 sin = sys.stdin.readlines()
 result = kks.convert((text if len(sin)==0 else "".join(sin)).strip())
 print("".join([f"{macro}:{it['orig']}[{it['hira']}]" 
